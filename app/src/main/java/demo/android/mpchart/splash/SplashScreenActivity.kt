99502e4f59881e1f4a0acc7cc8b3cb4009e58355
Plain <28282306+plain-dev@users.noreply.github.com>
2021-07-23 09:33:56 +0800

    🎨 Change app icon

--- app/src/main/java/demo/android/mpchart/splash/SplashScreenActivity.kt
+++ app/src/main/java/demo/android/mpchart/splash/SplashScreenActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.view.ViewCompat
+import androidx.core.view.WindowCompat
 import demo.android.mpchart.MainActivity
 import demo.android.mpchart.R
 
@@ -11,6 +12,7 @@ class SplashScreenActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
         setContentView(R.layout.activity_splash_screen)
         ViewCompat.postOnAnimationDelayed(window.decorView, {
             startActivity(Intent(this, MainActivity::class.java))
